8b8ee25826f2119186435877e4413e767e1468c7
filip7557 <filip.curic@student.ferit.hr>
2023-10-26 09:49:39 +0200

    Add repeat to ppz1.py

--- lab1/ppz1.py
+++ lab1/ppz1.py
@@ -1,6 +1,8 @@
 name = input("Unesi ime: ")
 age = int(input("Unesi godine: "))
+repeat = int(input("Unesi broj ponavljanja: "))
 
 age100 = 2023 - age + 100
 
-print(f"Bok {name}, imat ćeš 100 godina {age100} godine.")
+for _ in range(repeat):
+    print(f"Bok {name}, imat ćeš 100 godina {age100} godine.")
