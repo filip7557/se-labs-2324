a39869c103cd6617aee692afbaf3f9d6865b8037
filip7557 <filip.curic@student.ferit.hr>
2023-10-26 10:27:03 +0200

    Add extras to ppz5.py

--- lab1/ppz5.py
+++ lab1/ppz5.py
@@ -1,9 +1,20 @@
-a = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89]
-b = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13]
+import random
+
+def generate_random_list(low, high):
+    a = []
+    num_elements = random.randint(5, 15)
+    for _ in range(num_elements):
+        a.append(random.randint(low, high))
+    return a
+
+a = generate_random_list(1, 10)
+b = generate_random_list(3, 13)
 commons = []
 
 for el in a:
     if el in b and el not in commons:
         commons.append(el)
 
+print(a)
+print(b)
 print(commons)
