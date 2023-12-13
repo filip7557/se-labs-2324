286b43245c58f6e4c034ce8f59c558e0aee70107
filip7557 <filip.curic@student.ferit.hr>
2023-12-13 13:08:57 +0100

    Add image list

--- lab5/myimgur/images/models.py
+++ lab5/myimgur/images/models.py
@@ -5,3 +5,6 @@ class Image(models.Model):
     title = models.CharField(max_length=128)
     url = models.CharField(max_length=512)
     pub_date = models.DateTimeField()
+
+    def __str__(self):
+        return f"{self.id}: {self.title}"
--- lab5/myimgur/images/views.py
+++ lab5/myimgur/images/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from .models import Image
+
 # Create your views here.
 def home(request):
     context = {}
     return render(request, 'images/home.html', context)
 
 def image_list(request):
-    context = {}
+    images = Image.objects.all()
+    context = {
+        'images': images,
+    }
     return render(request, 'images/list.html', context)
