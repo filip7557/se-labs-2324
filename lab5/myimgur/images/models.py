d3e4c815da4c88dd58ae974fd42117bf48ead8c3
filip7557 <filip.curic@student.ferit.hr>
2023-12-13 12:40:56 +0100

    Add migrations

--- lab5/myimgur/images/models.py
+++ lab5/myimgur/images/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
 # Create your models here.
+class Image(models.Model):
+    title = models.CharField(max_length=128)
+    url = models.CharField(max_length=512)
+    pub_date = models.DateTimeField()
