a68015fc8d800cb32568a05dd43a01546aaf5610
filip7557 <filip.curic@student.ferit.hr>
2023-12-20 12:52:31 +0100

    Add Comment model

--- lab6/images/models.py
+++ lab6/images/models.py
@@ -8,3 +8,9 @@ class Image(models.Model):
 
     def __str__(self):
         return f"{self.id}: {self.title}"
+    
+class Comment(models.Model):
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
+    text = models.CharField(max_length=512)
+    name = models.CharField(max_length=128)
+    created_at = models.DateTimeField(auto_now_add=True)
