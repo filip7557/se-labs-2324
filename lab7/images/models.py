910992c865d0901eaa0a61c44b80c7c8840a95aa
filip7557 <filip.curic@student.ferit.hr>
2024-01-10 11:42:56 +0100

    Add user to comment

--- lab7/images/models.py
+++ lab7/images/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 class Image(models.Model):
@@ -10,9 +11,9 @@ class Image(models.Model):
         return f"{self.id}: {self.title}"
     
 class Comment(models.Model):
-    image= models.ForeignKey(Image, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    image = models.ForeignKey(Image, on_delete=models.CASCADE)
     text=models.CharField(max_length=512)
-    name=models.CharField(max_length=128)
     created_at=models.DateTimeField(auto_now_add=True)
     
     def __str__(self):
